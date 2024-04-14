cf0c2594eda65b041a25ac9890edf090d1a02de8
boomyao <boomyao@outlook.com>
2024-04-14 08:55:02 +0800

    Add command to create new app

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -4,8 +4,45 @@ import { build } from './build-fn.js';
 import { Command } from 'commander';
 import { checkBunInstallation } from './bun.js';
 import { execSync } from 'node:child_process';
+import fs from 'node:fs/promises';
+import { existsSync } from 'node:fs';
+import path from 'node:path';
 
 const program = new Command();
+
+program
+  .command('new')
+  .description('Create a new app')
+  .argument('[appName]', 'app name')
+  .action(async (appName) => {
+    try {
+      const templatePath = path.join(import.meta.url, '../template');
+      const targetPath = path.join(process.cwd(), appName);
+
+      if (existsSync(targetPath)) {
+        console.error('The app already exists!');
+        return;
+      }
+
+      await fs.cp(templatePath, targetPath, { recursive: true });
+
+      await fs.rename(
+        path.join(targetPath, '_gitignore'),
+        path.join(targetPath, '.gitignore'),
+      );
+
+      const packageJsonPath = path.join(targetPath, 'package.json');
+      const packageJson = JSON.parse(
+        await fs.readFile(packageJsonPath, 'utf-8'),
+      );
+      packageJson.name = appName;
+      await fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2));
+
+      console.log('App created successfully!');
+    } catch (error) {
+      console.error('Failed to create app:', error);
+    }
+  });
 
 program
   .command('dev')
---
+++ packages/cli/template/index.ts
@@ -0,0 +1,9 @@
+import { createApp } from '@pupa/app';
+
+const app = createApp({
+  name: '',
+  description: '',
+  icon: '',
+});
+
+app.serve();
